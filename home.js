eadebed6c680a0ddd2ee0095583bbb48bcc4a13e
group6coding <adrianextra40@Gmail.com>
2025-01-07 17:56:13 +0800

    Add files via upload

---
+++ home.js
@@ -0,0 +1,139 @@
+document.body.style.backgroundImage = "url()";
+function opendropdown() {
+    var content = document.querySelector('.dropdown .content');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+
+function opendropdown2() {
+    var content = document.querySelector('.dropdown2 .content2');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+/*Promo*/
+function openpromocard() {
+    var content = document.querySelector('.promocard .description');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+function openpromocard2() {
+    var content = document.querySelector('.promocard2 .description2');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+function openpromocard3() {
+    var content = document.querySelector('.promocard3 .description3');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+function openpromocard4() {
+    var content = document.querySelector('.promocard4 .description4');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+function openpromocard5() {
+    var content = document.querySelector('.promocard5 .description5');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+function openpromocard6() {
+    var content = document.querySelector('.promocard6 .description6');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+function openpromocard7() {
+    var content = document.querySelector('.promocard7 .description7');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+function openpromocard8() {
+    var content = document.querySelector('.promocard8 .description8');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+function openpromocard9() {
+    var content = document.querySelector('.promocard9 .description9');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+v
+function openpromocard10() {
+    var content = document.querySelector('.promocard10 .description10');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+function openpromocard11() {
+    var content = document.querySelector('.promocard11 .description11');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+function openpromocard12() {
+    var content = document.querySelector('.promocard12 .description12');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
+function openpromocard13() {
+    var content = document.querySelector('.promocard13 .description13');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
